dd5007568fef0732a7535b9465a098f232bd2f19
portal-gumelar <sukarnoanomali69@gmail.com>
2026-06-13 03:21:36 +0700

    feat: add apple touch startup images for PWA splash screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={inter.variable}>
+      <head>
+        {/* Splash Screen iPhone */}
+        <link rel="apple-touch-startup-image" media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1290,h-2796,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1179,h-2556,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1170,h-2532,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1125,h-2436,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-828,h-1792,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-750,h-1334,fo-auto,c-pad_resize,bg-1B2E52" />
+
+        {/* Splash Screen iPad */}
+        <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-2048,h-2732,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1668,h-2388,fo-auto,c-pad_resize,bg-1B2E52" />
+        <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="https://ik.imagekit.io/Sgd/sgd.png?tr=w-1536,h-2048,fo-auto,c-pad_resize,bg-1B2E52" />
+      </head>
       <body className="bg-background text-charcoal antialiased min-h-screen">
         <SessionProvider>
           <ThemeProvider
